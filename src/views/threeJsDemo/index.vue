3cebb9dea874efb444a4b9bddc482e1c317599d9
perryLinke <778520330@qq.com>
2024-01-02 17:32:43 +0800

    1

--- src/views/threeJsDemo/index.vue
+++ src/views/threeJsDemo/index.vue
@@ -4,7 +4,9 @@
 
 <script>
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls";
+// 引入扩展库OrbitControls.js
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
 export default {
   mounted() {
     // 创建场景
@@ -12,13 +14,13 @@ export default {
     const scene = new THREE.Scene();
 
     // 添加背景色
-    // scene.background = new THREE.Color(0x666666);
-    scene.background = new THREE.CubeTextureLoader()
-      .setPath("/")
-      .load(["01.png", "01.png", "01.png", "01.png", "01.png", "01.png"]);
+    scene.background = new THREE.Color(0x666666);
+    // scene.background = new THREE.CubeTextureLoader()
+    //   .setPath("/")
+    //   .load(["01.png", "01.png", "01.png", "01.png", "01.png", "01.png"]);
 
     // 添加雾
-    scene.fog = new THREE.Fog(0xcccccc, 10, 15);
+    // scene.fog = new THREE.Fog(0xcccccc, 10, 15);
 
     // 创建相机
     const camera = new THREE.PerspectiveCamera();
@@ -26,12 +28,36 @@ export default {
     camera.position.z = 10;
 
     // 创建立方体
+    // const geometry = new THREE.BoxGeometry(1, 6, 2);
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    // const material = new THREE.MeshBasicMaterial({
+    const material = new THREE.MeshLambertMaterial({
+      color: 0x00ff00, //设置材质颜色
+      transparent: true, //开启透明
+      opacity: 0.5, //设置透明度
+    });
 
-    // 网格
+    const pointLight = new THREE.PointLight(0xffffff, 1.0);
+    //点光源位置
+    pointLight.position.set(0, 1, 0); //点光源放在x轴上
+    pointLight.decay = 0.0; //设置光源不随距离衰减
+
+    // 你可以对比不同光照强度明暗差异(传播同样距离)
+    pointLight.intensity = 10000.0; //光照强度
+    pointLight.intensity = 50000.0; //光照强度
+    scene.add(pointLight);
+
+    const pointLightHelper = new THREE.PointLightHelper(pointLight, 1);
+    scene.add(pointLightHelper);
+
+    //环境光:没有特定方向，整体改变场景的光照明暗
+    const ambient = new THREE.AmbientLight(0xffffff, 0.4);
+    scene.add(ambient);
+
+    // 网格模型
     const cube = new THREE.Mesh(geometry, material);
     cube.position.set(3, 3, 3);
+    // cube.position.set(0, 0, 0);
     scene.add(cube);
 
     // 添加网格地面
@@ -40,7 +66,8 @@ export default {
 
     // 创建渲染器
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth - 180, window.innerHeight - 80);
+    // renderer.setSize(window.innerWidth - 180, window.innerHeight - 80);
+    renderer.setSize(1000,1000);
 
     // 将染器添加到页面
     // document.body.appendChild(renderer.domElement);
@@ -50,6 +77,7 @@ export default {
     const controls = new OrbitControls(camera, renderer.domElement);
     // 对轨道控制器改变时候进行监听
     controls.addEventListener("change", function () {
+      // console.log('camera.position',camera.position);
       //   console.log("触发change");
     });
 
@@ -61,7 +89,7 @@ export default {
     controls.autoRotateSpeed = 0.5;
 
     // 添加坐标轴
-    const axesHelper = new THREE.AxesHelper(5);
+    const axesHelper = new THREE.AxesHelper(10);
     axesHelper.position.y = 3;
     scene.add(axesHelper);
 
@@ -69,10 +97,12 @@ export default {
     renderer.render(scene, camera);
 
     // 让立方体动起来
+    // let i = 0;
     function animate() {
       //   requestAnimationFrame(animate);
       //   cube.rotation.x += 0.01;
-
+      // i += 1;
+      // console.log("执行次数" + i);
       requestAnimationFrame(animate);
       //   cube.rotation.x + 0.01;
       //   cube.rotation.y += 0.01;
@@ -83,6 +113,18 @@ export default {
     }
 
     animate();
+
+    // 渲染循环
+    // const clock = new THREE.Clock();
+    // function render() {
+    //   const spt = clock.getDelta() * 1000; //毫秒
+    //   console.log("两帧渲染时间间隔(毫秒)", spt);
+    //   console.log("帧率FPS", 1000 / spt);
+    //   renderer.render(scene, camera); //执行渲染操作
+    //   cube.rotateY(0.01); //每次绕y轴旋转0.01弧度
+    //   requestAnimationFrame(render); //请求再次执行渲染函数render，渲染下一帧
+    // }
+    // render();
   },
 };
 </script>
