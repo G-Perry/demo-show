68bf6734e08ddd60ba83d4c649f213a4f216420c
perryLinke <778520330@qq.com>
2024-01-05 11:49:28 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,8 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  devServer: {
+    port: 9999,
+    open: true,
+  },
 });
