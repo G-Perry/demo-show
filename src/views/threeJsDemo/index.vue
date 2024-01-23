cc142ab871273f34ecbc6f7c1a90041a6917319d
perryLinke <778520330@qq.com>
2024-01-23 17:29:39 +0800

    1

--- src/views/threeJsDemo/index.vue
+++ src/views/threeJsDemo/index.vue
@@ -1,229 +1,160 @@
 <template>
-  <div class="son_fit_father" ref="three_demo"></div>
+  <div ref="son_fit_father" class="son_fit_father" style="position: relative;">
+    <!-- 
+      设置两层是为了window.resize时，three_demo不被canvas高度撑开
+      设置绝对定位，并转而获取父级元素的宽高
+      以保证three_demo始终撑满却又不溢出
+    -->
+    <div
+      ref="three_demo"
+      style="position: absolute;width: 100%;height: 100%;"
+      tabindex="0"
+      @mousedown="handleMouseDown"
+      @mouseup="handleMouseUp"
+      @mousemove="handleMouseMove"
+    ></div>
+    <!-- 
+      @keydown="handleKeyDown"
+      @keyup="handleKeyUp"
+      @wheel.stop="handleWheel"
+    -->
+  </div>
 </template>
-  
-  <script>
-  import * as THREE from "three";
-  // 引入扩展库OrbitControls.js
-  import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-  // import dat from "dat.gui";
-  export default {
-    mounted() {
-      // 创建控制对象
-      const controlData = {
-        rotationSpeed: 0.02,
-        color: "#66ccff",
-        wireframe: false,
-      };
-      // 创建gui实例
-      // const gui = new dat.GUI();
-      // const f = gui.addFolder("配置");
-      // f.add(controlData, "rotationSpeed").min(0.01).max(0.1).step(0.01);
-      // f.addColor(controlData, "color");
-      // f.add(controlData, "wireframe");
-      // f.domElement.id = "gui";
-      // f.open();
-      // 创建场景
-      // 场景能够让你在什么地方、摆放什么东西来交给three.js渲染,这是你放置物体\灯光\摄像机的地方
-      const scene = new THREE.Scene();
-  
-      // 添加背景色
-      scene.background = new THREE.Color(0x666666);
-      // scene.background = new THREE.CubeTextureLoader()
-      //   .setPath("/")
-      //   .load(["01.png", "01.png", "01.png", "01.png", "01.png", "01.png"]);
-  
-      // 添加雾
-      // scene.fog = new THREE.Fog(0xcccccc, 10, 15);
-  
-      // 创建相机
-      const camera = new THREE.PerspectiveCamera();
-      camera.position.y = 5;
-      camera.position.z = 10;
-  
-      // // 创建立方体
-      // // const geometry = new THREE.BoxGeometry(1, 6, 2);
-      // const geometry = new THREE.BoxGeometry(1, 1, 1);
-      // // const material = new THREE.MeshBasicMaterial({
-      // const material = new THREE.MeshLambertMaterial({
-      //   color: 0x00ff00, //设置材质颜色
-      //   transparent: true, //开启透明
-      //   opacity: 0.5, //设置透明度
-      // });
-  
-      const pointLight = new THREE.PointLight(0xffffff, 1.0);
-      //点光源位置
-      pointLight.position.set(0, 1, 0); //点光源放在x轴上
-      pointLight.decay = 0.0; //设置光源不随距离衰减                    
-  
-      // 你可以对比不同光照强度明暗差异(传播同样距离)
-      pointLight.intensity = 10000.0; //光照强度
-      pointLight.intensity = 50000.0; //光照强度
-      scene.add(pointLight);
-  
-      const pointLightHelper = new THREE.PointLightHelper(pointLight, 1);
-      scene.add(pointLightHelper);
-  
-      //环境光:没有特定方向，整体改变场景的光照明暗
-      const ambient = new THREE.AmbientLight(0xffffff, 0.4);
-      scene.add(ambient);
-  
-      // // 网格模型
-      // const cube = new THREE.Mesh(geometry, material);
-      // cube.position.set(3, 3, 3);
-      // // cube.position.set(0, 0, 0);
-      // scene.add(cube);
-  
-      // const geometry = new THREE.BoxGeometry(1, 1, 1);
-      // //材质对象Material
-      // const material = new THREE.MeshLambertMaterial({
-      //   color: 0x00ffff, //设置材质颜色
-      //   transparent: true, //开启透明
-      //   opacity: 0.5, //设置透明度
-      // });
-      // for (let i = 0; i < 10; i++) {
-      //   const mesh = new THREE.Mesh(geometry, material); //网格模型对象Mesh
-      //   // 沿着x轴分布
-      //   mesh.position.set(i * 2, i, i);
-      //   scene.add(mesh); //网格模型添加到场景中
-      // }
-  
-      //创建一个长方体几何对象Geometry
-      const geometry = new THREE.BoxGeometry(1, 2, 1);
-      //BoxGeometry：长方体
-      // const geometry = new THREE.BoxGeometry(100, 100, 100);
-      // SphereGeometry：球体
-      // const geometry = new THREE.SphereGeometry(50);
-      // CylinderGeometry：圆柱
-      // const geometry = new THREE.CylinderGeometry(50, 50, 100);
-      // PlaneGeometry：矩形平面
-      // const geometry = new THREE.PlaneGeometry(100, 50);
-      // CircleGeometry：圆形平面
-      // const geometry = new THREE.CircleGeometry(50);
-      //材质对象Material
-      // const material = new THREE.MeshLambertMaterial({
-      //   color: 0x00ffff, //设置材质颜色
-      //   transparent: true, //开启透明
-      //   opacity: 0.5, //设置透明度
-      // });
-      // const material = new THREE.MeshPhongMaterial({
-      //   color: 0xff0000,
-      //   shininess: 20, //高光部分的亮度，默认30
-      // });
-      // 模拟镜面反射，产生一个高光效果
-      // const material = new THREE.MeshPhongMaterial({
-      //   color: 0xff0000,
-      //   shininess: 20, //高光部分的亮度，默认30
-      //   specular: 0x444444, //高光部分的颜色
-      // });
-  
-      // for (let i = 0; i < 10; i++) {
-      //   for (let j = 0; j < 10; j++) {
-      //     const mesh = new THREE.Mesh(geometry, material); //网格模型对象Mesh
-      //     // 在XOZ平面上分布
-      //     mesh.position.set(i * 2, 0, j * 2);
-      //     scene.add(mesh); //网格模型添加到场景中
-      //   }
-      // }
-  
-      // 点渲染模式
-      const material = new THREE.PointsMaterial({
-        color: 0xffff00,
-        size: 1.0, //点对象像素尺寸
-      });
-  
-      const points = new THREE.Points(geometry, material); //点模型对象
-      scene.add(points);
-  
-      // 添加网格地面
-      const gridHelper = new THREE.GridHelper(10, 10);
-      scene.add(gridHelper);
-  
-      // 创建渲染器
-      // const renderer = new THREE.WebGLRenderer();
-    //   const renderer = new THREE.WebGLRenderer({
-    //     antialias: true,
-    //   });
-    //   renderer.setSize(window.innerWidth - 180, window.innerHeight - 80);
-      // renderer.setSize(1000, 1000);
-      const renderer = new THREE.WebGLRenderer();
-      // renderer.setSize(window.innerWidth - 180, window.innerHeight - 80);
-      renderer.setSize(1000, 1000);
-  
-      // 将染器添加到页面
-      // document.body.appendChild(renderer.domElement);
-      this.$refs.three_demo.appendChild(renderer.domElement);
-      // this.$refs.three_demo.appendChild(f.domElement);
-  
-      // 添加轨道控制器
-      const controls = new OrbitControls(camera, renderer.domElement);
-      // 对轨道控制器改变时候进行监听
-      controls.addEventListener("change", function () {
-        // console.log('camera.position',camera.position);
-        //   console.log("触发change");
-      });
-  
-      // 添加阻尼
-      controls.enableDamping = true;
-      controls.dampingFactor = 0.1;
-      // 自动旋转
-      controls.autoRotate = true;
-      controls.autoRotateSpeed = 0.5;
-  
-      // 添加坐标轴
-      const axesHelper = new THREE.AxesHelper(10);
-      // axesHelper.position.y = 3;
-      scene.add(axesHelper);
-  
-      // 进行渲染
-      renderer.render(scene, camera);
-  
-      // 让立方体动起来
-      // let i = 0;
-      function animate() {
-        //   requestAnimationFrame(animate);
-        //   cube.rotation.x += 0.01;
-        // i += 1;
-        // console.log("执行次数" + i);
-        requestAnimationFrame(animate);
-        //   cube.rotation.x + 0.01;
-        //   cube.rotation.y += 0.01;
-        //   cube.rotation.z += 0.01;
-        // cube.rotation.x += controlData.rotationSpeed;
-        // cube.material.color = new THREE.Color(controlData.color);
-        material.wireframe = controlData.wireframe;
-        // 轨道控制器更新
-        controls.update();
-        renderer.render(scene, camera);
-      }
-  
-      animate();
-  
-      // 渲染循环
-      // const clock = new THREE.Clock();
-      // function render() {
-      //   const spt = clock.getDelta() * 1000; //毫秒
-      //   console.log("两帧渲染时间间隔(毫秒)", spt);
-      //   console.log("帧率FPS", 1000 / spt);
-      //   renderer.render(scene, camera); //执行渲染操作
-      //   cube.rotateY(0.01); //每次绕y轴旋转0.01弧度
-      //   requestAnimationFrame(render); //请求再次执行渲染函数render，渲染下一帧
-      // }
-      // render();
-      // onresize 事件会在窗口被调整大小时发生
-      window.onresize = function () {
-        // 重置渲染器输出画布canvas尺寸
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        // 全屏情况下：设置观察范围长宽比aspect为窗口宽高比
-        camera.aspect = window.innerWidth / window.innerHeight;
-        // 渲染器执行render方法的时候会读取相机对象的投影矩阵属性projectionMatrix
-        // 但是不会每渲染一帧，就通过相机的属性计算投影矩阵(节约计算资源)
-        // 如果相机的一些属性发生了变化，需要执行updateProjectionMatrix ()方法更新相机的投影矩阵
-        camera.updateProjectionMatrix();
-      };
-      console.log("查看当前屏幕设备像素比", window.devicePixelRatio);
+
+<script>
+// 引入threeJS
+import * as THREE from "three";
+// 引入GLTF加载器
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+// 引入轨道控制器扩展库OrbitControls.js
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+export default {
+  data() {
+    return {
+      width: 0,
+      height: 0,
+      scene: null,
+      camera: null,
+      renderer: null,
+      loader: null,
+      controls: null,
+      clock: null,
+      STEPS_PER_FRAME: 5,
+    };
+  },
+  methods: {
+    getWidthHeight() {
+      // 获取容器的宽高
+      this.width = this.$refs.son_fit_father.offsetWidth;
+      this.height = this.$refs.son_fit_father.offsetHeight;
     },
-  };
-  </script>
-  
-  <style></style>
+    init() {
+      // 该对象用于跟踪时间
+      this.clock = new THREE.Clock();
+      // 初始化场景
+      this.scene = new THREE.Scene();
+      this.scene.background = new THREE.Color(0x88ccee); //  设置背景色
+      this.scene.fog = new THREE.Fog(0x88ccee, 0, 50); //  设置场景雾
+
+      // 初始化相机
+      this.camera = new THREE.PerspectiveCamera(
+        70, // fov 相机视锥体竖直方向视野角度
+        this.width / this.height, // aspect相机视锥体水平方向和竖直方向长度比，一般设置为Canvas画布宽高比width / height
+        0.1, // near	相机视锥体近裁截面相对相机距离
+        1000 // far	相机视锥体远裁截面相对相机距离，far-near构成了视锥体高度方向
+      );
+      this.camera.rotation.order = "YXZ";
+      this.camera.position.z = 5;
+
+      // 初始化渲染器
+      this.renderer = new THREE.WebGLRenderer({ antialias: true }); //抗锯齿
+      this.renderer.setPixelRatio(window.devicePixelRatio); //设置设备像素比。通常用于避免HiDPI设备上绘图模糊
+      this.renderer.setSize(this.width, this.height); //将输出canvas的大小调整为(width, height)
+      this.renderer.shadowMap.enabled = true; //设置开启，允许在场景中使用阴影贴图
+      this.renderer.shadowMap.type = THREE.VSMShadowMap; //使用Variance Shadow Map (VSM)算法来过滤阴影映射。当使用VSMShadowMap时，所有阴影接收者也将会投射阴影。
+      this.renderer.toneMapping = THREE.ACESFilmicToneMapping; //色调映射的曝光级别,ACES Filmic Tone Mapping 主要用于模拟电影胶片的色调映射效果，提供更自然、更富有动感的渲染结果。
+      this.$refs.three_demo.appendChild(this.renderer.domElement);
+
+      // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+      // 初始化平行光
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 2.5);
+      directionalLight.position.set(-5, 25, -1);
+      directionalLight.castShadow = true;
+      directionalLight.shadow.camera.near = 0.01;
+      directionalLight.shadow.camera.far = 500;
+      directionalLight.shadow.camera.right = 30;
+      directionalLight.shadow.camera.left = -30;
+      directionalLight.shadow.camera.top = 30;
+      directionalLight.shadow.camera.bottom = -30;
+      directionalLight.shadow.mapSize.width = 1024;
+      directionalLight.shadow.mapSize.height = 1024;
+      directionalLight.shadow.radius = 4;
+      directionalLight.shadow.bias = -0.00006;
+      this.scene.add(directionalLight);
+
+      // 半球光（HemisphereLight）光源直接放置于场景之上，光照颜色从天空光线颜色渐变到地面光线颜色。
+      const fillLight = new THREE.HemisphereLight(0x8dc1de, 0x00668d, 1.5);
+      fillLight.position.set(2, 1, 1);
+      this.scene.add(fillLight);
+      // 加载glb类型的建模
+      this.loader = new GLTFLoader().setPath("./threeJsModel/gltf/");
+      this.loader.load("collision-world.glb", (gltf) => {
+        this.scene.add(gltf.scene);
+      });
+    },
+    animate() {
+      /**
+       * this.clock.getDelta()方法返回自上一次调用以来经过的时间（秒）
+       * Math.min(0.05, ...)：这一部分确保 deltaTime 不会过大，限制在 0.05 秒内。这是为了防止由于性能原因导致的巨大的时间间隔，从而造成物理模拟和动画的不稳定性。
+       * STEPS_PER_FRAME 将 deltaTime 分成多个小步骤，以便在每个小步骤中进行部分更新。这可以帮助提高模拟的稳定性，特别是在动画和物理计算中。
+       * 这一行代码的目的是计算一个适当的小时间步 deltaTime，以确保在每一帧内都有合适的时间来进行模拟和动画的更新，而不至于出现过大的变化。
+       */
+      const deltaTime =
+        Math.min(0.05, this.clock.getDelta()) / this.STEPS_PER_FRAME;
+
+      this.renderer.render(this.scene, this.camera);
+      requestAnimationFrame(this.animate);
+      // this.controls.update();
+    },
+    onWindowResize() {
+      this.getWidthHeight();
+      this.camera.aspect = this.width / this.height;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(this.width, this.height);
+    },
+    handleMouseDown(e) {
+      document.body.requestPointerLock();
+
+      console.log(e, document.body);
+    },
+    handleMouseUp() {},
+    handleMouseMove(e) {
+      if (document.pointerLockElement === document.body) {
+        console.log(e);
+        this.camera.rotation.y -= e.movementX / 500;
+        this.camera.rotation.x -= e.movementY / 500;
+      }
+    },
+    handleKeyDown(e) {
+      console.log(111, e);
+    },
+    handleKeyUp() {},
+  },
+  mounted() {
+    this.getWidthHeight();
+    this.init();
+    this.animate();
+    window.addEventListener("resize", this.onWindowResize);
+    document.addEventListener("keydown", (event) => {
+      console.log(222, event, document.pointerLockElement);
+      // keyStates[event.code] = true;
+    });
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onWindowResize);
+  },
+};
+</script>
+
+<style>
+</style>
